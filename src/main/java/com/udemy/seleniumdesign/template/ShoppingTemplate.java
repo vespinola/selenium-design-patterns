dd1e76992bdf820988b20759c73c8b8706fc1533
Vladimir Espinola <vladimir.espinola@gmail.com>
2024-08-14 15:55:32 -0400

    added template base example

---
+++ src/main/java/com/udemy/seleniumdesign/template/ShoppingTemplate.java
@@ -0,0 +1,16 @@
+package com.udemy.seleniumdesign.template;
+
+public abstract class ShoppingTemplate {
+    public abstract void launchSite();
+    public abstract void searchForProduct();
+    public abstract void selectProduct();
+
+    public abstract void buy();
+
+    public void shop() {
+        launchSite();
+        searchForProduct();
+        selectProduct();
+        buy();
+    }
+}
