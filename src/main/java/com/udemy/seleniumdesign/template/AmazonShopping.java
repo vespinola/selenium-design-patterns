a347a7aa2afd68f8abf15f5c175b20fcd4a5f5a8
Vladimir Espinola <vladimir.espinola@gmail.com>
2024-08-14 16:09:36 -0400

    created amazon page

---
+++ src/main/java/com/udemy/seleniumdesign/template/AmazonShopping.java
@@ -0,0 +1,59 @@
+package com.udemy.seleniumdesign.template;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AmazonShopping extends ShoppingTemplate {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private String product;
+
+    @FindBy(id = "twotabsearchtextbox")
+    private WebElement searchBox;
+
+    @FindBy(css = "inout.nav-input")
+    private WebElement searchBtn;
+
+    @FindBy(css = "span.a-size-medium")
+    private WebElement item;
+
+    @FindBy(id = "priceblock ourprice")
+    private WebElement price;
+
+    public AmazonShopping(WebDriver driver, String product) {
+        this.driver = driver;
+        this.product = product;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        PageFactory.initElements(driver, this);
+    }
+
+    @Override
+    public void launchSite() {
+        this.driver.get("https://www.amazon.com/");
+    }
+
+    @Override
+    public void searchForProduct() {
+        this.searchBox.sendKeys(product);
+        this.searchBtn.click();
+    }
+
+    @Override
+    public void selectProduct() {
+        this.wait.until((d) -> this.item.isDisplayed());
+        this.item.click();
+    }
+
+    @Override
+    public void buy() {
+        this.wait.until((d) -> this.price.isDisplayed());
+        System.out.println(
+                this.price.getText()
+        );
+    }
+}
