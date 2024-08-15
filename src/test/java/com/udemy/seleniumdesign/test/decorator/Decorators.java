010ec01063d21834c3fad24d51e8962c4dd11ef2
Vladimir Espinola <vladimir.espinola@gmail.com>
2024-08-15 15:22:41 -0400

    added ingredients for decorator test

--- src/test/java/com/udemy/seleniumdesign/test/decorator/Decorators.java
+++ src/test/java/com/udemy/seleniumdesign/test/decorator/Decorators.java
@@ -20,14 +20,30 @@ public class Decorators {
     private static final Consumer<DashboardPage> adminComponentPresent = (dp) -> shouldDisplay(dp.getAdminComponents());
     private static final Consumer<DashboardPage> adminComponentNotPresent = (dp) -> shouldNotDisplay(dp.getAdminComponents());
 
-    private static final Consumer<DashboardPage> guestComponentPresence = (dp) -> shouldDisplay(dp.getGuestComponents());
+    private static final Consumer<DashboardPage> guestComponentPresent = (dp) -> shouldDisplay(dp.getGuestComponents());
     private static final Consumer<DashboardPage> guestComponentNotPresent = (dp) -> shouldNotDisplay(dp.getGuestComponents());
 
-    private static final Consumer<DashboardPage> superuserComponentPresence = (dp) -> shouldDisplay(dp.getSuperuserComponents());
-    private static final Consumer<DashboardPage> superuserComponentNotPresent = (dp) -> shouldNotDisplay(dp.getSuperuserComponents());
+    private static final Consumer<DashboardPage> suComponentPresent = (dp) -> shouldDisplay(dp.getSuperuserComponents());
+    private static final Consumer<DashboardPage> suComponentNotPresent = (dp) -> shouldNotDisplay(dp.getSuperuserComponents());
 
     //role selection
     private static final Consumer<DashboardPage> adminSelection = dashboardPage -> dashboardPage.selectRole("admin");
-    private static final Consumer<DashboardPage> superuserSelection = dashboardPage -> dashboardPage.selectRole("superuser");
+    private static final Consumer<DashboardPage> suSelection = dashboardPage -> dashboardPage.selectRole("superuser");
     private static final Consumer<DashboardPage> guestSelection = dashboardPage -> dashboardPage.selectRole("guest");
+
+    //user role pages
+    public static final Consumer<DashboardPage> guestPage = guestSelection
+            .andThen(guestComponentPresent)
+            .andThen(suComponentPresent)
+            .andThen(adminComponentNotPresent);
+
+    public static final Consumer<DashboardPage> suPage = suSelection
+            .andThen(guestComponentPresent)
+            .andThen(suComponentPresent)
+            .andThen(adminComponentNotPresent);
+
+    public static final Consumer<DashboardPage> adminPage = adminSelection
+            .andThen(guestComponentPresent)
+            .andThen(suComponentPresent)
+            .andThen(adminComponentPresent);
 }
